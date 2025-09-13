34378081158b031dbc03dc9a02516a2b6a3c9c58
Shreya Srivastava <shreyasrivastava722@gmail.com>
2025-09-14 01:01:30 +0530

    changes jwt to env to access secrets

--- middleware/user.js
+++ middleware/user.js
@@ -1,10 +1,10 @@
 const jwt=require("jsonwebtoken")
-const {JWT_USER_PASSWORD}=process.env.JWT_USER_PASSWORD
+
 
 
 function userMiddleware(req,res,next){
     const token=req.headers.token;
-    const decoded=jwt.verify(token,JWT_USER_PASSWORD)
+    const decoded=jwt.verify(token,process.env.JWT_USER_PASSWORD)
 
     if(decoded){
         req.userId=dedcoded._id
